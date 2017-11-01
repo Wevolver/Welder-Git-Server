a3e745c04deec888319dbcf87df9510a541319a8
Ben <ben@wevolver.com>
2017-11-01 14:11:08 +0000

    less than 5 commit hotfix

--- welder/notifications/decorators.py
+++ welder/notifications/decorators.py
@@ -29,11 +29,14 @@ def notify(action):
             for branch in repo.branches:
                 walker = repo.walk(repo.revparse_single(branch).id, pygit2.GIT_SORT_TIME)
                 existing_commits[branch] = []
-                for i in range(5):
-                    existing_commits[branch].append(next(walker).tree_id)
+                try:
+                    for i in range(5):
+                        existing_commits[branch].append(next(walker).tree_id)
+                except:
+                    logger.info('less than 5 commits')
 
             to_return = func(request, *args, **kwargs)
-            branch = sorted(repo.branches, key=lambda x:repo.revparse_single(x).commit_time)[1]
+            branch = sorted(repo.branches, key=lambda x:repo.revparse_single(x).commit_time)[-1]
             access_token = request.META.get('HTTP_AUTHORIZATION', None)
             access_token = access_token if access_token else request.GET.get("access_token")
 
@@ -49,14 +52,17 @@ def notify(action):
 
             events = []
             walker = repo.walk(repo.revparse_single(branch).id, pygit2.GIT_SORT_TIME)
-            for i in range(5):
-                commit = next(walker)
-                if commit.tree_id not in existing_commits[branch]:
-                    events.append({
-                      "who": commit.committer.email,
-                      "what": commit.message,
-                      "where": branch
-                    })
+            try:
+                for i in range(5):
+                    commit = next(walker)
+                    if commit.tree_id not in existing_commits[branch]:
+                        events.append({
+                          "who": commit.committer.email,
+                          "what": commit.message,
+                          "where": branch
+                        })
+            except:
+                logger.info('less than 5')
 
             if(access_token):
                 send_notification(user_name, project_name, action, access_token, events)
