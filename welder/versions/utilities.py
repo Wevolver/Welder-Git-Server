132a91d887241050d23aa88f586f7846f1fb5af2
Ben <test@email.com>
2018-03-09 13:48:49 -0800

    fixes the commit splitting to use . with a following space

--- welder/versions/utilities.py
+++ welder/versions/utilities.py
@@ -8,15 +8,19 @@ import os
 
 logger = logging.getLogger(__name__)
 
+
 def fetch_repository(username, projectname):
     directory = porcelain.generate_directory(username)
     source_path = os.path.join(settings.REPO_DIRECTORY, directory, projectname)
     repo = pygit2.Repository(source_path)
     return repo
 
-def split_commit_message(commit_message):
-    splits = [50,
-              (commit_message.index('\n') if '\n' in commit_message else float("inf")),
-              (commit_message.index('.') if '.' in commit_message else float("inf"))]
 
-    return [commit_message[:min(splits)], commit_message[min(splits):]]
+def split_commit_message(commit_message):
+    splits = [
+        50, (commit_message.index('\n') if '\n' in commit_message else
+             float("inf")), (commit_message.index('. ')
+                             if '. ' in commit_message else float("inf"))
+    ]
+
+    return [commit_message[:min(splits) + 1], commit_message[min(splits) + 1:]]
