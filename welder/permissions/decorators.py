a15b0246a3364a71f7f8b9dd8b80ceeaed24eaec
Ben Carothers <ben@carothers.io>
2019-10-28 12:13:54 -0400

    default read with no permissions

--- welder/permissions/decorators.py
+++ welder/permissions/decorators.py
@@ -50,7 +50,7 @@ def requires_permission_to(permission):
                 success, response = get_token(user_name, project_name, access_token, url=host_url)
                 decoded_token = {  "project": "default", "permissions": "['read']"}
                 token = 'default'
-                if(response.get('content', False)):
+                if(response['content']):
                     token = response.content
                     decoded_token = decode_token(token)
                 permissions = decoded_token['permissions'] if decoded_token else ''
